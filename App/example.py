287ae7baa0a1fef44bad808b023d5533d43c7943
Nevul <alexmaxter1232@gmail.com>
2024-02-21 13:03:03 -0500

    Cambio de nombre - principal.py -> main.py

--- App/example.py
+++ App/example.py
@@ -1,6 +1,6 @@
-import App.principal as principal
+import App.main as main
 
 #Si importamos el módulo, este archivo se ejecutará así solo llamemos una variable.
 #Por ello es importante modularizar el código en funciones. Revisar el archivo main.py
-print(principal.data)
-principal.run()
+print(main.data)
+main.run()

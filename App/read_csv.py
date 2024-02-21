077b9b63ff8ba0f22d64a4553a37a18af8e363ce
Nevul <alexmaxter1232@gmail.com>
2024-02-21 12:23:50 -0500

    lectura de csv - transformación de csv a un modo más legible

---
+++ App/read_csv.py
@@ -0,0 +1,16 @@
+import csv
+def read_csv(path):
+    with open(path) as csvfile:
+        reader = csv.reader(csvfile, delimiter = ',')   #delimiter es el separador de los campos en el archivo csv
+        header = next(reader)   #leemos la 1ra fila que contiene los nombres de las columnas
+        data = []
+        for row in reader:
+            iterable = zip(header, row)     #combina los nombres de las columnas con los valores de las filas
+            data_dict = {key: value for key, value in iterable} #dict comprehension que extrae los pares key: value del iterable
+            data.append(data_dict)  #añade cada diccionario a la lista vacía
+        return data #retorna una lista de diccionarios
+
+#Permite la ejecución del archivo como script desde una consola
+if __name__ == '__main__':
+    data = read_csv('App/data.csv')
+    print(data)

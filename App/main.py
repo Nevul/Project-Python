611acc2dcd1e6d084c5c73475a26fbcb61a9ead8
Nevul <alexmaxter1232@gmail.com>
2024-02-23 08:57:20 -0500

    Gráfico de población por años - Extracción de datos

--- App/main.py
+++ App/main.py
@@ -1,31 +1,20 @@
 import utils
+import read_csv
+import charts
 
 #Al estar modularizado el código en funciones, aunque ejecutemos este archivo, no se ejecutrá nada de su código interno.
 
-data = [
-        {
-            'country': 'Ecuador',
-            'population': 18000000
-        },
-        {
-            'country': 'Perú',
-            'population': 33000000
-        },
-        {
-            'country': 'Colombia',
-            'population': 51000000
-        }
-    ]
 
-def run():   
-    countries, population = utils.get_population()
-    print(countries, population)
-    print(utils.name)
-    print(utils.age)
-    print(utils.sex)
 
-    country = input('Escriba un país: ')
-    print(utils.population_by_country(data, country))
+def run():
+    data = read_csv.read_csv('App/data.csv')
+    country = input('País: ')
+    country_list_dict = utils.population_by_country(data, country)
+
+    if len(country_list_dict) > 0:
+        country_dict = country_list_dict[0]
+        labels, values = utils.get_population_year(country_dict)
+        charts.generate_bar_chart(labels, values)
 
 #Con esto se puede ejecutar el código del archivo, aunque esté modularizado en funciones
     #'__main__' es independiente del nombre del archivo.
--- App/utils.py
+++ App/utils.py
@@ -1,10 +1,25 @@
+def get_population_year(country_dict):
+    population_dict = {
+        '2022': int(country_dict['2022 Population']),
+        '2020': int(country_dict['2020 Population']),
+        '2015': int(country_dict['2015 Population']),
+        '2010': int(country_dict['2010 Population']),
+        '2000': int(country_dict['2000 Population']),
+        '1990': int(country_dict['1990 Population']),
+        '1980': int(country_dict['1980 Population']),
+        '1970': int(country_dict['1970 Population']),
+    }
+    labels = population_dict.keys()
+    values = population_dict.values()
+    return labels, values
+
 def get_population():
     countries = ['ecu', 'col', 'per']
     population = [18, 51, 33]
     return countries, population
 
 def population_by_country(data, country):
-    result = list(filter(lambda item: item['country'] == country, data))
+    result = list(filter(lambda item: item['Country/Territory'] == country, data))
     return result
 
 name = 'Alex'

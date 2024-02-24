a4f6978588aaaa784408462f26c2922632192c8a
Nevul <alexmaxter1232@gmail.com>
2024-02-24 16:18:54 -0500

    Reto de gráfica mundial por países

---
+++ App/population_percentage.py
@@ -0,0 +1,10 @@
+import read_csv
+import charts
+
+data = read_csv.read_csv('App/data.csv')
+data = list(filter(lambda data_dict: data_dict['Continent'] == 'Asia', data))
+
+countries = list(map(lambda data_dict: data_dict['Country/Territory'], data))
+percetanges = list(map(lambda data_dict: data_dict['World Population Percentage'], data))
+
+charts.generate_pie_chart(countries, percetanges)

d9e4e28713eb39ee3ebc2bbbb13d51c50012f649
Nevul <alexmaxter1232@gmail.com>
2024-02-26 08:34:58 -0500

    Save images - gitignore - main.py

--- App/charts.py
+++ App/charts.py
@@ -3,13 +3,15 @@ import matplotlib.pyplot as plt
 def generate_bar_chart(labels, values):
     fig, ax = plt.subplots()
     ax.bar(labels, values)
-    plt.show()
+    plt.savefig('./saves/bar.png')
+    plt.close()
 
 def generate_pie_chart(labels, values):
     fig, ax = plt.subplots()
     ax.pie(values, labels = labels)
     ax.axis('equal')    #Revisar, parece que ya viene por defecto este valor al ser un pie chart
-    plt.show()
+    plt.savefig('./saves/pie.png')
+    plt.close()
 
 if __name__ == '__main__':
     labels = ['laptop', 'mouse', 'router']
--- App/main.py
+++ App/main.py
@@ -3,11 +3,14 @@ import read_csv
 import charts
 
 #Al estar modularizado el código en funciones, aunque ejecutemos este archivo, no se ejecutrá nada de su código interno.
-
-
-
 def run():
-    data = read_csv.read_csv('App/data.csv')
+    data = read_csv.read_csv('./data.csv')
+
+    data = list(filter(lambda data_dict: data_dict['Continent'] == 'Asia', data))
+    countries = list(map(lambda data_dict: data_dict['Country/Territory'], data))
+    percetanges = list(map(lambda data_dict: data_dict['World Population Percentage'], data))
+    charts.generate_pie_chart(countries, percetanges)
+
     country = input('País: ')
     country_list_dict = utils.population_by_country(data, country)
 

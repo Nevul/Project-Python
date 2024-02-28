2e12f50144e31f447a6de0012fd07cd0358f8717
Nevul <alexmaxter1232@gmail.com>
2024-02-28 18:56:46 -0500

    Implement library Pandas in App/main.py

--- App/main.py
+++ App/main.py
@@ -1,17 +1,32 @@
 import utils
 import read_csv
+import pandas as pd
 import charts
 
 #Al estar modularizado el código en funciones, aunque ejecutemos este archivo, no se ejecutrá nada de su código interno.
 def run():
+    '''
+    #-----------------option 1-----------------
     data = read_csv.read_csv('./data.csv')
-
-    continent = 'Asia'
+    continent = input('Continente: ')
 
     data = list(filter(lambda data_dict: data_dict['Continent'] == continent, data))
     countries = list(map(lambda data_dict: data_dict['Country/Territory'], data))
     percetanges = list(map(lambda data_dict: data_dict['World Population Percentage'], data))
     charts.generate_pie_chart(continent, countries, percetanges)
+    #------------------End option 1-----------------
+    '''
+    #------------------option 2-----------------
+    df = pd.read_csv('./data.csv')
+    continent = input('Continente: ')
+    df = df[df['Continent'] == continent]
+
+    countries = df['Country/Territory'].values
+    percetanges = df['World Population Percentage'].values
+    
+    charts.generate_pie_chart(continent, countries, percetanges)
+    data = read_csv.read_csv('./data.csv')
+    #------------------End option 2-----------------
 
     country = input('País: ')
     country_list_dict = utils.population_by_country(data, country)

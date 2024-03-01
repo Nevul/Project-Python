f2917180dc1ea1f95e0aeb27ad7df5e9bcaec830
Nevul <alexmaxter1232@gmail.com>
2024-02-29 23:53:29 -0500

    Web server with FastAPI, response HTML and execution with Uvicorn

--- web-server/main.py
+++ web-server/main.py
@@ -1,7 +1,30 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI() #instancia de la aplicación
+
+@app.get('/')   #ubicación desde la que pueden acceder desde la web
+def get_list(): #resultaod a mostrar al ser solicitado
+    return [1, 2, 3]
+
+@app.get('/contact', response_class=HTMLResponse)   #ubicación de acceso desde la web y respuesta de tipo HTML ante solicitudes
+def get_list():
+    return '''
+    <html>
+        <head>
+            <title>Page HTML</title>
+        </head>
+        <body>
+            <h1>Esto es un HTML</h1>
+        </body>
+    </html>
+    '''
 
 def run():
     store.get_categories()
 
 if __name__ == '__main__':
     run()
+
+#Para ejecutar el servidor web, usar: 'uvicorn main:app --reload' en la terminal.

5632f469b3f854e94bb42024b1b7bd4c412cfb08
Nevul <alexmaxter1232@gmail.com>
2024-03-01 23:32:40 -0500

    Vinculación de directorio a Contenedor y Verificación

--- App/population_percentage.py
+++ App/population_percentage.py
@@ -2,7 +2,7 @@ import read_csv
 import charts
 
 data = read_csv.read_csv('./data.csv')
-data = list(filter(lambda data_dict: data_dict['Continent'] == 'Asia', data))
+data = list(filter(lambda data_dict: data_dict['Continent'] == 'South America', data))
 
 countries = list(map(lambda data_dict: data_dict['Country/Territory'], data))
 percetanges = list(map(lambda data_dict: data_dict['World Population Percentage'], data))
